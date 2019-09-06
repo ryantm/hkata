1103b667dddc42f30b6eeecf883ec47393035913
Ryan Mulligan <ryan@ryantm.com>
2019-09-05 17:22:58 -0700

    setup Kata for https://www.codewars.com/kata/vowel-count/train/haskell

--- Kata.hs
+++ Kata.hs
@@ -1,13 +1,8 @@
 module Kata where
 
-import Control.Category ((>>>))
-import Data.Char (toLower)
-import Data.Function ((&))
-import Data.List (sort)
-
--- | True if the string is an isogram
+-- | Return the number of vowels given a string
 --
--- >>> isIsogram ""
--- True
-isIsogram :: String -> Bool
-isIsogram = undefined
+-- >>> getCount ""
+-- 0
+getCount :: String -> Int
+getCount = undefined
